28a2c640e77eed597a7b61b8f4a320f4ac08564a
billyjackhouze <billyjacksimpson@gmail.com>
2026-05-05 10:50:34 -0600

    Add legacy plugin for Samsung TV browser compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['chrome >= 56', 'samsung >= 8', 'safari >= 11'],
+    }),
+  ],
 
   server: {
     port: 5173,
